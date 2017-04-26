7e582b67cfa78682b4c4301c8efbf5f99747f93b
Brittany1908 <b.nicoleoneil@gmail.com>
2017-04-25 20:42:34 -0400

    Update basicWebScraperURL.py

--- basicWebScraperURL.py
+++ basicWebScraperURL.py
@@ -1,15 +1,20 @@
 '''
 Program Name: DCA Scraper
 Created: March 12, 2017
-Modified: March 15, 2017
+Modified: April 25, 2017
 Purpose: Obtain information from websites, then parse the
 information.
 '''
 #-----------------------------------------------------------
-''' Import urllib and BeautifulSoup 4 for obtaining
+''' Import csv, urllib, and BeautifulSoup 4 for obtaining
 website info and then parsing''' 
+import csv;
 import urllib.request;
 from bs4 import BeautifulSoup;
+
+#-----------------------------------------------------------
+#Open File
+resultFile = open("output.csv", 'w')
 
 #-----------------------------------------------------------
 # Assigning variables
@@ -20,8 +25,15 @@ website = urllib.request.urlopen('http://www.5dca.org/Opinions/Opin2017/030617/f
 soup = BeautifulSoup(website, 'html.parser');
 
 #-----------------------------------------------------------
-# Print all links as a sting.
-for link in soup.findAll('a'):
-    print (link.string);
+# Print all links as a string for visual and defines data 
+for link in soup.find_all('a'):    
+    print(link.string);    
+    # write to file    
+    results = [link.string];    
+    for everyCase in results:        
+        resultFile.write(everyCase + "\n")
+#-----------------------------------------------------------  
+# Close file
+resultFile.close()
 
 # Currently only written opinions are printed. Not PCAs
